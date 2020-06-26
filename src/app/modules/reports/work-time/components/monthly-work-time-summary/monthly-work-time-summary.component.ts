98763985f40ebadda475e91d0c0fed792abd22ff
matjanko <mateusz.jankowski@prokonto.pl>
2020-06-26 18:49:46 +0200

    Refactor work time again

--- src/app/modules/reports/work-time/components/monthly-work-time-summary/monthly-work-time-summary.component.ts
+++ src/app/modules/reports/work-time/components/monthly-work-time-summary/monthly-work-time-summary.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MonthlyWorkTime } from '../../models/monthly-work-time';
 
 @Component({
   selector: 'app-monthly-work-time-summary',
   templateUrl: './monthly-work-time-summary.component.html',
-  styleUrls: ['./monthly-work-time-summary.component.less']
+  styleUrls: ['./monthly-work-time-summary.component.less'],
 })
 export class MonthlyWorkTimeSummaryComponent implements OnInit {
+  @Input() selectedMonthlyWorkTime: MonthlyWorkTime;
+  @Input() selectedDate: Date;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
